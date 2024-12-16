4212dea7ad293e1be2f204b1d8db0df46c13dacd
odwillio0702 <odwillio21100@gmail.com>
2024-12-16 16:43:46 +0100

    1

---
+++ script.js
@@ -0,0 +1,9 @@
+const elements = document.querySelectorAll('.fade-in');
+window.addEventListener('scroll', () => {
+    elements.forEach(el => {
+        const rect = el.getBoundingClientRect();
+        if (rect.top < window.innerHeight) {
+            el.classList.add('visible');
+        }
+    });
+});
